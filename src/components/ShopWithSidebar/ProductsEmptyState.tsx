3caf7b4972485017aa51a99685bf4a7ee846543b
Chance Weston <chanceweston97@gmail.com>
2025-12-17 05:11:39 -0600

    shop page fix

--- src/components/ShopWithSidebar/ProductsEmptyState.tsx
+++ src/components/ShopWithSidebar/ProductsEmptyState.tsx
@@ -3,7 +3,7 @@ export default function ProductsEmptyState() {
   return (
     <div className="flex items-center justify-center">
       <p className="text-center text-2xl py-5">
-        No products found!
+        No products found!!!
       </p>
     </div>
   )
